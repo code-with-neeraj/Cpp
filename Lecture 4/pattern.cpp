7ce98ddf082f8fa9e5ddaeaa600de68dd811bef3
code-with-neeraj <neerajkr145518@gmail.com>
2025-07-05 07:50:08 +0530

    add pattern

---
+++ Lecture 4/pattern.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    ////////////////////////////////////
+
+    // 1234
+    // 1234
+    // 1234
+    // 1234
+
+    // int n =4;
+
+    // for(int i=1; i<=n; i++){
+
+    //     for(int j=1; j<=n; j++ ){
+    //     cout << j << " ";
+    //     }
+
+    //     cout << endl;
+
+    // }
+
+    /////////////////////////////////////
+
+    // A B C D 
+    // A B C D
+    // A B C D
+    // A B C D
+
+    //  int n =4;
+
+    // for(int i=0; i<n; i++){
+
+    //     char ch ='A';
+    //     for(int j=0; j<n; j++ ){
+    //     cout << ch << " ";
+    //     ch = ch + 1;
+    //     }
+
+    //     cout << endl;
+    // }
+
+    // //////////////////////////////////////
+
+    // 1 2 3 
+    // 4 5 6
+    // 7 8 9
+
+    // int n = 3;
+    // int num = 1;
+
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<n; j++){
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    ////////////////////////////////
+    // *
+    // **
+    // ***
+    // ****
+
+    // int n = 4;
+
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<i+1; j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    return 0;
+}
