e8cc56c8df96b11aa94c93ae277781ff29ad6807
code-with-neeraj <neerajkr145518@gmail.com>
2025-07-15 19:59:01 +0530

    bitwise Operators

---
+++ Lecture 7/BitwiseOperators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a = 4, b = 8;
+
+    cout << (a & b) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+
+
+    cout << (4 << 1) << endl;
+    cout << (4 >> 1) << endl;
+
+
+    return 0;
+}
---
+++ Lecture 7/dataType.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a = 4, b = 8;
+
+    cout << sizeof(int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(short int) << endl;
+
+    cout << sizeof(long long int) << endl;
+  
+
+
+    return 0;
+}
