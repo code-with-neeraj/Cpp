ee588af277cfa83705566e7bec6afe163257d0ef
code-with-neeraj <neerajkr145518@gmail.com>
2025-08-21 19:50:34 +0530

    learn aggressive cows problem

---
+++ Lecture 23/aggressiveCows_pro.cpp
@@ -0,0 +1,52 @@
+// Aggressive Cows Problem
+
+// Assign C cows to N stalls such that min distance between them is alrgest possible.
+// Return largest minimum distance.
+
+// N=5 arr=[1,2,8, 4,9] C=3
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPossible(vector<int> &arr, int N, int C, int minAllowedDist){ //0(N)
+    int cows = 1, lastStallPos = arr[0];
+
+    for(int i=1; i<N; i++){
+        if(arr[i]-lastStallPos >= minAllowedDist){
+            cows++;
+            lastStallPos = arr[i];
+        }
+
+        if(cows == C){
+            return true;
+        }
+    }
+    return false;
+}
+
+int getDistance(vector<int> &arr, int N, int C) {
+    sort(arr.begin(), arr.end());  // NlogN
+
+    int st = 1, end = arr[N-1]-arr[0], ans = -1;
+
+    while(st <= end){  //0(log(Range) * N)
+        int mid = st + (end - st) /2;
+
+        if(isPossible(arr, N, C, mid)){ //right
+            ans = mid;
+            st = mid + 1;
+        } else { //left
+            end = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main(){
+    int N = 5, C = 3;
+    vector<int> arr = {1, 2, 8, 4, 9};
+
+    cout << getDistance(arr, N, C) << endl;
+}
